e35ded10c03768d9c8f3e3d30630aaa9687c490e
Kevin <kevinthelago@gmail.com>
2024-11-02 02:08:11 -0400

    About

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,9 +1,14 @@
-// import AboutCard from '../components/about-card/AboutCard';
-
 const About = (props) => {
     return(
         <div className='page gallery-theme'>
-            {/* <AboutCard artist={props.artist}/> */}
+            <div className="about-card">
+                <div className='about-card-description'>
+                    {props.artist.about.split("\n").map(text => <h4>{text}</h4>)}
+                </div>
+                <div className='about-card-image-wrapper'>
+                    <img className='about-image' alt='' src='https://images.pexels.com/photos/2381069/pexels-photo-2381069.jpeg?auto=compress&cs=tinysrgb&w=600'/>
+                </div>
+            </div>
         </div>
     )
 }
