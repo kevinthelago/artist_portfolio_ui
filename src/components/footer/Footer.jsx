5766ca123538210e9d852087f443d70b7c632f34
Kevin <kevinthelago@gmail.com>
2024-11-03 12:23:37 -0500

    Stuff

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,6 +3,12 @@ import './footer.css';
 const Footer = (props) => {
     return(
         <div className='page footer'>
+            <div className='footer-nav'>
+
+            </div>
+            <div className='footer-links'>
+
+            </div>
             footer
         </div>
     )
